4001fad7f4195d0f16c45c44789318315ea1da09
Impact-I <87244850+Impact-I@users.noreply.github.com>
2022-04-16 00:55:32 +0300

    Create frida.js

---
+++ frida.js
@@ -0,0 +1,65 @@
+//frida -U -f <package> -l frida.js --no-pause
+
+function hookFunc() {
+
+    var dumpOffset = '0x20801C' // _kDartIsolateSnapshotInstructions + code offset
+
+    var argBufferSize = 150
+
+    var address = Module.findBaseAddress('libapp.so') // libapp.so (Android) or App (IOS) 
+    console.log('\n\nbaseAddress: ' + address.toString())
+
+    var codeOffset = address.add(dumpOffset)
+    console.log('codeOffset: ' + codeOffset.toString())
+    console.log('')
+    console.log('Wait..... ')
+
+    Interceptor.attach(codeOffset, {
+        onEnter: function(args) {
+
+            console.log('')
+            console.log('--------------------------------------------|')
+            console.log('\n    Hook Function: ' + dumpOffset);
+            console.log('')
+            console.log('--------------------------------------------|')
+            console.log('')
+
+            for (var argStep = 0; argStep < 50; argStep++) {
+                try {
+                    dumpArgs(argStep, args[argStep], argBufferSize);
+                } catch (e) {
+
+                    break;
+                }
+
+            }
+
+
+
+
+        },
+        onLeave: function(retval) {
+            console.log('RETURN : ' + retval)
+            dumpArgs(0, retval, 150);
+        }
+    });
+
+}
+
+function dumpArgs(step, address, bufSize) {
+
+    var buf = Memory.readByteArray(address, bufSize)
+
+    console.log('Argument ' + step + ' address ' + address.toString() + ' ' + 'buffer: ' + bufSize.toString() + '\n\n Value:\n' +hexdump(buf, {
+        offset: 0,
+        length: bufSize,
+        header: false,
+        ansi: false
+    }));
+
+    console.log('')
+    console.log('----------------------------------------------------')
+    console.log('')
+}
+
+setTimeout(hookFunc, 1000)
