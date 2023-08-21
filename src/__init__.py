5792fc54a4858658ada7ba12e212e30fbcec4307
Jay <jay@sperixlabs.org>
2023-08-21 00:31:28 +0000

    Patch dump

--- src/__init__.py
+++ src/__init__.py
@@ -301,7 +301,7 @@ def patchSource(hashS, ver):
         replaceFileText('src/third_party/dart/runtime/vm/dart_api_impl.cc', 'FLAG_print_class_table)', 'true)')
         replaceFileText('src/third_party/dart/runtime/vm/class_table.cc', '#include "vm/visitor.h"',
                         '#include "vm/visitor.h"\n#include <sys/stat.h>')
-        replaceFileText('src/third_party/dart/runtime/vm/class_table.cc', 'print_class_table, false', 'print_class_table, true')
+        #replaceFileText('src/third_party/dart/runtime/vm/class_table.cc', 'print_class_table, false', 'print_class_table, true')
 
     if ver > 27:
         replaceFileText('src/flutter/BUILD.gn',
