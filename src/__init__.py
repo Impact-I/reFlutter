3df82236064a156a3d60080e106325f86ae59a4a
Jay <jay@sperixlabs.org>
2023-11-25 23:32:59 +0000

    test dump dart update

--- src/__init__.py
+++ src/__init__.py
@@ -279,18 +279,23 @@ def patchSource(hashS, ver):
         replaceFileText('src/third_party/dart/runtime/vm/app_snapshot.cc', 'monomorphic_entry_point + unchecked_offset',
                         'previous_text_offset_')
     if ver > 38 and patchDump:
-        replaceFileText('src/third_party/dart/runtime/vm/app_snapshot.cc', 'monomorphic_entry_point + unchecked_offset',
-                        'instructions_table_.rodata()->entries()[instructions_table_.rodata()->first_entry_with_code + instructions_index_-1].pc_offset')
+        # replaceFileText('src/third_party/dart/runtime/vm/app_snapshot.cc', 'monomorphic_entry_point + unchecked_offset', 'instructions_table_.rodata()->entries()[instructions_table_.rodata()->first_entry_with_code + instructions_index_-1].pc_offset')
+        replaceFileText('src/third_party/dart/runtime/vm/app_snapshot.cc', """code->untag()->monomorphic_unchecked_entry_point_ =
+      monomorphic_entry_point + unchecked_offset;""", """auto& offset = instructions_table_.rodata()->entries()[instructions_table_.rodata()->first_entry_with_code + instructions_index_ - 1].pc_offset;
+  code->untag()->monomorphic_unchecked_entry_point_ = offset;""")
+
+        # new fix for patch dump
+        replaceFileText('src/third_party/dart/runtime/vm/app_snapshot.cc', 'ASSERT(code->IsCode());',
+                        'ASSERT(code->IsCode());\n auto& rCode = Code::Handle(code); auto& rClass = Class::Handle(func.Owner()); auto& rLib = Library::Handle(rClass.library()); auto& rlibName = String::Handle(rLib.url()); auto& offset  = func.ptr()->untag()->unchecked_entry_point_(); JSONWriter js; js.OpenObject(); js.PrintProperty("method_name", func.UserVisibleNameCString()); js.PrintProperty("offset", offset); auto& sig = String::Handle(func.InternalSignature()); js.PrintProperty("library_url", rlibName.ToCString()); js.PrintProperty("class_name", rClass.UserVisibleNameCString()); js.CloseObject(); char* buffer = nullptr; intptr_t buffer_length = 0; js.Steal(&buffer, &buffer_length); struct stat entry_info; int exists = 0; if (stat("/data/data/", &entry_info)==0 && S_ISDIR(entry_info.st_mode)){ exists = 1; } if(exists == 1){ pid_t pid = getpid(); char path[64] = { 0 }; snprintf(path, sizeof(path), "/proc/%d/cmdline", pid); FILE *cmdline = fopen(path, "r"); if (cmdline) { char chm[264] = { 0 }; char pat[264] = { 0 }; char application_id[64] = { 0 }; fread(application_id, sizeof(application_id), 1, cmdline); snprintf(pat, sizeof(pat), "/data/data/%s/dump.dart", application_id); do { FILE *f = fopen(pat, "a+"); fprintf(f, "%s", buffer); fflush(f); fclose(f); snprintf(chm, sizeof(chm), "/data/data/%s",application_id); chmod(chm, S_IRWXU|S_IRWXG|S_IRWXO); chmod(pat, S_IRWXU|S_IRWXG|S_IRWXO); } while (0); fclose(cmdline); } } if(exists == 0){ char pat[264] = { 0 }; snprintf(pat, sizeof(pat), "%s/Documents/dump.dart", getenv("HOME")); OS::PrintErr("reFlutter dump file: %s",pat); do { FILE *f = fopen(pat, "a+"); fprintf(f, "%s", buffer); fflush(f); fclose(f); } while (0); }\n')
+
     if patchDump:
         replaceFileText('src/third_party/dart/runtime/vm/dart.cc', 'FLAG_print_class_table)', 'true)')
         replaceFileText('src/third_party/dart/runtime/vm/dart_api_impl.cc', 'FLAG_print_class_table)', 'true)')
         replaceFileText('src/third_party/dart/runtime/vm/class_table.cc', '#include "vm/visitor.h"',
                         '#include "vm/visitor.h"\n#include <sys/stat.h>')
+        replaceFileText('src/third_party/dart/runtime/vm/app_snapshot.cc', '#include "vm/version.h"',
+                        '#include "vm/version.h"\n#include <sys/stat.h>')
         # replaceFileText('src/third_party/dart/runtime/vm/class_table.cc', 'print_class_table, false', 'print_class_table, true')
-
-        # new fix for patch dump
-        replaceFileText('src/third_party/dart/runtime/vm/app_snapshot.cc', 'ASSERT(code->IsCode());',
-                        'ASSERT(code->IsCode());\n auto& rCode = Code::Handle(code); auto& rClass = Class::Handle(func.Owner()); auto& rLib = Library::Handle(rClass.library()); auto& rlibName = String::Handle(rLib.url()); JSONWriter js; js.OpenObject(); js.PrintProperty("method_name", func.UserVisibleNameCString()); js.PrintProperty("offset", offset); auto& sig = String::Handle(func.InternalSignature()); js.PrintProperty("library_url", rlibName.ToCString()); js.PrintProperty("class_name", rClass.UserVisibleNameCString()); js.CloseObject(); char* buffer = nullptr; intptr_t buffer_length = 0; js.Steal(&buffer, &buffer_length); struct stat entry_info; int exists = 0; if (stat("/data/data/", &entry_info)==0 && S_ISDIR(entry_info.st_mode)){ exists = 1; } if(exists == 1){ pid_t pid = getpid(); char path[64] = { 0 }; snprintf(path, sizeof(path), "/proc/%d/cmdline", pid); FILE *cmdline = fopen(path, "r"); if (cmdline) { char chm[264] = { 0 }; char pat[264] = { 0 }; char application_id[64] = { 0 }; fread(application_id, sizeof(application_id), 1, cmdline); snprintf(pat, sizeof(pat), "/data/data/%s/dump.dart", application_id); do { FILE *f = fopen(pat, "a+"); fprintf(f, "%s", buffer); fflush(f); fclose(f); snprintf(chm, sizeof(chm), "/data/data/%s",application_id); chmod(chm, S_IRWXU|S_IRWXG|S_IRWXO); chmod(pat, S_IRWXU|S_IRWXG|S_IRWXO); } while (0); fclose(cmdline); } } if(exists == 0){ char pat[264] = { 0 }; snprintf(pat, sizeof(pat), "%s/Documents/dump.dart", getenv("HOME")); OS::PrintErr("reFlutter dump file: %s",pat); do { FILE *f = fopen(pat, "a+"); fprintf(f, "%s", buffer); fflush(f); fclose(f); } while (0); }\n')
 
     if ver > 27:
         replaceFileText('src/flutter/BUILD.gn',
