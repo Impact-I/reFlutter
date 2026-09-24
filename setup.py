02bb2fd024f436f73926d2cc755418f4a8782e76
Jay <jay@sperixlabs.org>
2026-09-24 05:23:41 +0000

    0.9.5: idempotent include anchors + solution-repo reset (final-audit findings)

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ CLASSIFIERS = [
 
 setuptools.setup(
     name="reflutter",
-    version="0.9.4",
+    version="0.9.5",
     description=SHORT_DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
