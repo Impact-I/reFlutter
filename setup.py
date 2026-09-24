1b5d688ffc00896d3c466797913a99fc846e4d38
Jay <jay@sperixlabs.org>
2026-09-24 07:11:28 +0000

    0.9.8

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ CLASSIFIERS = [
 
 setuptools.setup(
     name="reflutter",
-    version="0.9.7",
+    version="0.9.8",
     description=SHORT_DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
